8dee1aaf3ca73215423c51da0cf618f62b6bc09c
Montebo <28013382+mbmedhurst@users.noreply.github.com>
2019-04-16 18:40:43 -0700

    created files

---
+++ assets/javascript/golfPool.js
@@ -0,0 +1,5 @@
+// pga 2019 schedule
+// http://api.sportradar.us/golf-t2/schedule/pga/2019/tournaments/schedule.json?api_key=mfv63p7kr5859wt5ehmqkg7y
+
+// euro 2019 schedule
+// http://api.sportradar.us/golf-t2/schedule/euro/2019/tournaments/schedule.json?api_key=mfv63p7kr5859wt5ehmqkg7y
